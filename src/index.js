aff272d8458129c27cf8f89e1639fe4e358193d2
Murat AKMAMEDAU <muratx10@gmail.com>
2019-10-11 18:50:29 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,46 +1,69 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  
+  MORSE_TABLE[''] = ' ';
+  
+    let char_10 = /.{1,10}/g; //regexp to get all 10 paired characters
+    let char_2 = /\d{1,2}/g; //regexp to get all 2 paired characters
+    let whiteSpace = /\*{1,10}/g; //regexp to get * chars
+    
+    let letterArr = expr.match(char_10);
+    let result = '';
 
+    letterArr.forEach(element => {
+      let symbolsArr = !whiteSpace.test(element) ?
+        element.match(char_2) :
+        ['whitespace'];
+      
+      const decodedSymbols = symbolsArr.map(item => {
+        if (item === "10") return ".";
+        if (item === "11") return "-";
+        if (item === "whitespace") return '';
+        if (item === '00') return '';
+      });
+      
+      result += MORSE_TABLE[decodedSymbols.join('')]
+    });
+    return result;
+  }
 module.exports = {
-    decode
+  decode
 }
